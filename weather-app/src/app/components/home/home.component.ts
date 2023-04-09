e2b795ad44433ee13589f3c8a81e2b819dae30ee
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-04-09 02:49:58 +0200

    Bug fix

--- weather-app/src/app/components/home/home.component.ts
+++ weather-app/src/app/components/home/home.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {WeatherService} from "./weather.service";
-import {Forecast} from "./models/weather";
+import { HttpClient } from '@angular/common/http';
+import { WeatherService } from './weather.service';
+import { Forecast } from './models/weather';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  //forecast: Forecast;
-  constructor(private weatherService:WeatherService) {}
+export class HomeComponent implements OnInit {
+  
+  forecast: Forecast = {} as Forecast;
+
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    
+    this.getForecast();
   }
 
-  getForecast(){
-
+  getForecast() {
+    this.weatherService.getForecast('kara').subscribe((response) => {
+      this.forecast = response;
+    });
   }
 }
