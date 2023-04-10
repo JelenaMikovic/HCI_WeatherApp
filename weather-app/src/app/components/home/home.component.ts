7c52f23beb012839c8fd59a9063274a1ab077e51
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-04-10 22:20:04 +0200

    Added air quality

--- weather-app/src/app/components/home/home.component.ts
+++ weather-app/src/app/components/home/home.component.ts
@@ -136,6 +136,90 @@ export class HomeComponent implements OnInit {
     const day = String(today.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
+
+  calculateAqi(pm25: number): number {
+    const breakpoints = [0, 12.1, 35.5, 55.5, 150.5, 250.5, 350.5, 500.5];
+    const aqiRange = [0, 50, 100, 150, 200, 300, 400, 500];
+    
+    let i: number;
+    for (i = 0; i < breakpoints.length; i++) {
+      if (pm25 <= breakpoints[i]) {
+        break;
+      }
+    }
+    
+    if (i === 1) {
+      return Math.round(((aqiRange[i] - aqiRange[i - 1]) / (breakpoints[i] - breakpoints[i - 1])) * (pm25 - breakpoints[i - 1]) + aqiRange[i - 1]);
+    } else {
+      return Math.round(((aqiRange[i] - aqiRange[i - 1]) / (breakpoints[i] - breakpoints[i - 1])) * (pm25 - breakpoints[i - 1]) + aqiRange[i - 1]);
+    }
+  }
+
+  co2ToWidth(co2: number): string {
+    const maxCo2 = 5000;
+    const minCo2 = 0; 
+    const maxWidth = 100; 
+
+    const width = (co2 - minCo2) * maxWidth / (maxCo2 - minCo2);
+    
+    return `${width}%`;
+  }
+
+  getNo2ProgressBarWidth(): string {
+    const no2 = this.display.airQuality.no2;
+    let width: number;
+    
+    if (no2 <= 53) {
+      width = no2 / 53 * 25;
+    } else if (no2 <= 100) {
+      width = (no2 - 53) / 47 * 25 + 25;
+    } else if (no2 <= 360) {
+      width = (no2 - 100) / 260 * 25 + 50;
+    } else if (no2 <= 649) {
+      width = (no2 - 360) / 289 * 25 + 75;
+    } else if (no2 <= 1249) {
+      width = (no2 - 649) / 600 * 25 + 90;
+    } else {
+      width = 100;
+    }
+  
+    return `${width}%`;
+  }
+
+  getSO2Width(so2: number): string {
+    const value = Math.min(Math.floor(so2 / 20), 5) * 20;
+    return `${value}%`;
+  }
+
+  getPM2_5Width(pm2_5: number): string {
+    if (pm2_5 > 500) {
+      return '100%';
+    } else if (pm2_5 < 0) {
+      return '0%';
+    } else {
+      return Math.round(pm2_5 / 500 * 100) + '%';
+    }
+  }
+
+  getO3Width(o3Value: number): string {
+    if (o3Value > 300) {
+      return '100%';
+    } else if (o3Value < 0) {
+      return '0%';
+    } else {
+      return Math.round(o3Value / 300 * 100) + '%';
+    }
+  }
+
+  getPm10Width(pm10Value: number): string {
+    if (pm10Value > 500) {
+      return '100%';
+    } else if (pm10Value < 0) {
+      return '0%';
+    } else {
+      return Math.round(pm10Value / 500 * 100) + '%';
+    }
+  }
 }
 
 interface Display{
